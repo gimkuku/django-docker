68fefac5685d5938a5c0c2d3528507374fbe01be
julia-ing <ywchoi0625@gmail.com>
2021-09-15 23:39:11 +0900

    fix env vars

--- django_docker/settings/dev.py
+++ django_docker/settings/dev.py
@@ -5,7 +5,7 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DATABASE_DATABASE'),
+        'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASSWORD'),
         'HOST': env('DATABASE_HOST'),
